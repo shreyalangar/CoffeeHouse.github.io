2b67bb9ede1abae1a54aa5d450fbd07d390f6794
Shreya Langar <73985138+shreyalangar@users.noreply.github.com>
2021-07-14 13:38:08 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -20,4 +20,4 @@ mysqli_select_db($conn,"tyit")or die(mysqli_error());
 
 $sql="insert into feedback (contact_name, contact_email, contact_no, contact_subject, contact_message) VALUES ('$_POST[contact_name]','$_POST[contact_email]','$_POST[contact_no]','$_POST[contact_subject]','$_POST[contact_message]')";
 mysqli_query($conn,$sql)or die(mysqli_error($conn));
-
+?>
