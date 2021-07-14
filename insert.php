07b07acf25ede502ad055d2e89e2873b14acaa85
Shreya Langar <73985138+shreyalangar@users.noreply.github.com>
2021-07-14 13:37:05 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -21,27 +21,3 @@ mysqli_select_db($conn,"tyit")or die(mysqli_error());
 $sql="insert into feedback (contact_name, contact_email, contact_no, contact_subject, contact_message) VALUES ('$_POST[contact_name]','$_POST[contact_email]','$_POST[contact_no]','$_POST[contact_subject]','$_POST[contact_message]')";
 mysqli_query($conn,$sql)or die(mysqli_error($conn));
 
-//Display operation
-
-$res=mysqli_query($conn,"select * from feedback");
-print "<html><body><br/>";
-print "<b> Feedback Form </b>";
-echo "<table border='1'>";
-echo "<tr><td>Name</td><td>Email</td><td>Mobile No</td><td>Subject</td><td>Message</td>";
-while($row=mysqli_fetch_array($res))
-{
-echo "<tr><td>";
-echo $row['contact_name'];
-echo "</td><td>";
-echo $row['contact_email'];
-echo "</td><td>";
-echo $row['contact_no'];
-echo "</td><td>";
-echo $row['contact_subject'];
-echo "</td><td>";
-echo $row['contact_message'];
-echo "</td>";
-echo "</tr>";
-} 
-echo "</table></body></html>";
-?>
